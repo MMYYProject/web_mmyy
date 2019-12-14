6f63311d6e1dca0d0fbb53bd35ab712ab68a64e5
Crazysanlang <1356423746@qq.com>
2019-12-14 21:16:17 +0800

    提交

---
+++ src/settings.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: san lang
+ * @Date: 2019-11-21 15:08:06
+ * @LastEditTime: 2019-11-23 00:06:01
+ * @Description:
+ */
+module.exports = {
+
+  title: '商城后台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
